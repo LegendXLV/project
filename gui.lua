ba866b233c393c4ebef21d05869e4b9b43515ae0
LegendXLV <152641566+LegendXLV@users.noreply.github.com>
2024-12-31 16:21:03 +0100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,22 +1,24 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "discord: legendxlv", HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "legendxlv", HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
 
 --values
+
 _G.autoTap = true
 
 --functions
 
 function autoTap()
     while _G.autoTap == true do
-      game:GetService("ReplicatedStorage").Remotes.Tap:FireServer()
+    	game:GetService("ReplicatedStorage").Remotes.Tap:FireServer()
      end
     end
 
 --tabs
+
 local AutoTab = Window:MakeTab({
-	Name = "Auto Farm",
+	Name = "AutoFarm",
 	Icon = "rbxassetid://4483345998",
-   PremiumOnly = false
+    PremiumOnly = false
 })
 
 --toggles
@@ -25,11 +27,12 @@ AutoTab:AddToggle({
 	Name = "Auto Tap",
 	Default = false,
 	Callback = function(Value)
+		_G.autoTap = Value
 		autoTap()
 	end 	   
 })
 
 
 
-
+end
 OrionLib:Init()
